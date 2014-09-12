b90e28d828b3d785b53d613afdb9fcd32a293fd2
jonschlinkert <dev@sellside.com>
2014-09-12 04:42:22 -0400

    revisions to helper:
    * changes error messages to mention the name of the helper
    * usages `export.i18n` for compatibility with any setup
    
    Also:
    
    * adds tests
    * updates metadata to describe Laurent as author
    * updates readme/docs

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-'use strict';
-
 /**
- * {{i18n}}
- * @author: Laurent Goderre <https://github.com/LaurentGoderrre>
- * @param  {String} context
- * @param  {Object} options
- * @return {String}
- * @example: <https://github.com/assemble/buttons> (See the "button-i18n" example)
+ * {{i18n}} <http://github.com/helpers/handlebars-helper-i18n>
+ *
+ * Copyright (c) 2014 Laurent Goderre <https://github.com/LaurentGoderre>
+ * Licensed under the MIT License (MIT)
  */
 
-var i18n = function (context, options) {
-  var language = void 0;
-  if (typeof context !== "string") {
-    throw "Key must be of type 'string'";
-  }
-  language = (typeof options.hash.language === "string" ? options.hash.language : this.language);
-  if (typeof language === "undefined") {
-    throw "The 'language' parameter is not defined";
-  }
-  if (typeof this[language] === "undefined") {
-    throw "No strings found for language '" + language + "'";
-  }
-  if (typeof this[language][context] === "undefined") {
-    throw "No string for key '" + context + "' for language '" + language + "'";
-  }
-  return this[language][context];
-};
+'use strict';
 
-module.exports = i18n;
 
-module.exports.register = function(Handlebars, options) {
+/**
+ * @param  {String} `key` The name of the property to use as context.
+ * @param  {Object} `options`
+ * @return {Object}
+ */
+
+exports.i18n = function (key, options) {
   options = options || {};
+  options.hash = options.hash || {};
 
-  Handlebars.registerHelper('i18n', i18n);
+  var language;
+
+  if (typeof key !== "string") {
+    throw "{{i18n}} helper: invalid key. Object keys must be formatted as strings.";
+  }
+
+  if (typeof options.hash.language === "string") {
+    language = options.hash.language;
+  } else {
+    language = this.language;
+  }
+
+  if (typeof language === "undefined") {
+    throw "{{i18n}} helper: the 'language' parameter is not defined.";
+  }
+
+  if (typeof this[language] === "undefined") {
+    throw "{{i18n}} helper: context object not defined for language '" + language + "'.";
+  }
+
+  if (typeof this[language][key] === "undefined") {
+    throw "{{i18n}} helper: property '" + key + "' is not defined for language '" + language + "'.";
+  }
+
+  return this[language][key];
 };
--- test.js
+++ test.js
@@ -1,40 +1,69 @@
 /**
- * Handlebars Helpers Tests: i18n Helpers
- * http://github.com/assemble/handlebars-helpers
- * Copyright (c) 2013 Jon Schlinkert, Brian Woodward, contributors
- * Licensed under the MIT License (MIT).
+ * {{i18n}} <http://github.com/helpers/handlebars-helper-i18n>
+ *
+ * Copyright (c) 2014 Laurent Goderre <https://github.com/LaurentGoderre>
+ * Licensed under the MIT License (MIT)
  */
 
-// node_modules
-var expect = require('chai').expect;
+var should = require('should');
 var Handlebars = require('handlebars');
-
-// Local helpers
-var i18n = require('./index.js');
+var helpers = require('./index');
 
 var context = {
   language: 'en',
-  en: {
-    key: 'value'
-  },
-  fr: {
-    key: 'valeur'
-  }
+  en: {key: 'value'},
+  fr: {key: 'valeur'}
 };
 
-Handlebars.registerHelper("i18n", i18n);
+Handlebars.registerHelper("i18n", helpers.i18n);
+
 
 describe('i18n helper', function () {
-  describe('{{#i18n}}', function () {
-    it('should take a key and return for the default language', function () {
-      var source = '{{#i18n "key"}}{{/i18n}}';
-      var template = Handlebars.compile(source);
-      expect(template(context)).to.eql('value');
-    });
-    it('should take a key and return for the override language', function () {
-      var source = '{{#i18n "key" language="fr"}}{{/i18n}}';
-      var template = Handlebars.compile(source);
-      expect(template(context)).to.equal('valeur');
-    });
+  it('should take a key and return for the default language', function () {
+    var template = Handlebars.compile('{{i18n "key"}}');
+    template(context).should.eql('value');
+  });
+
+  it('should take a key and return for the override language', function () {
+    var template = Handlebars.compile('{{i18n "key" language="fr"}}');
+    template(context).should.equal('valeur');
+  });
+});
+
+describe('errors:', function () {
+  it('should throw an error when an invalid key is passed:', function () {
+    var template = Handlebars.compile('{{i18n foo}}');
+    try {
+      template();
+    } catch(err) {
+      err.should.equal("{{i18n}} helper: invalid key. Object keys must be formatted as strings.");
+    }
+  });
+
+  it('should throw an error when an empty context is passed.', function () {
+    var template = Handlebars.compile('{{i18n "key" language="fr"}}');
+    try {
+      template({});
+    } catch(err) {
+      err.should.equal("{{i18n}} helper: context object not defined for language 'fr'.");
+    }
+  });
+
+  it('should throw an error when no language is undefined.', function () {
+    var template = Handlebars.compile('{{i18n "key"}}');
+    try {
+      template({en: {key: 'value'}, fr: {key: 'valeur'} });
+    } catch(err) {
+      err.should.equal("{{i18n}} helper: the 'language' parameter is not defined.");
+    }
+  });
+
+  it('should throw an error when the given property is missing.', function () {
+    var template = Handlebars.compile('{{i18n "word" language="fr"}}');
+    try {
+      template({language: 'en', en: {word: 'value'}, fr: {}});
+    } catch(err) {
+      err.should.equal("{{i18n}} helper: property 'word' is not defined for language 'fr'.");
+    }
   });
 });
